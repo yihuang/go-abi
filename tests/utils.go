bde4576d52864d7daa133b192f6f5c6cb1dabf63
yihuang <yi.codeplayer@gmail.com>
2025-11-13 22:46:45 +0800

    cleanup

--- tests/utils.go
+++ tests/utils.go
@@ -3,9 +3,9 @@ package tests
 import (
 	"errors"
 	"io"
+	"slices"
 	"testing"
 
-	ethabi "github.com/ethereum/go-ethereum/accounts/abi"
 	"github.com/test-go/testify/require"
 	"github.com/yihuang/go-abi"
 )
@@ -31,12 +31,10 @@ func DecodeRoundTrip[T any, PT interface {
 	}
 
 	// test validation with bit flipping
-	// Test a selection of bit positions to verify validation is working
 	if len(data) > 0 {
 		// Test diverse positions across the entire data
 		for pos := 0; pos < len(data)*8; pos++ {
-			flipped := make([]byte, len(data))
-			copy(flipped, data)
+			flipped := slices.Clone(data)
 			bitIndex := pos / 8
 			bitOffset := pos % 8
 			flipped[bitIndex] ^= 1 << bitOffset
@@ -48,37 +46,6 @@ func DecodeRoundTrip[T any, PT interface {
 			if err == nil {
 				require.NotEqual(t, orig, &flippedDecoded, "orig: %v, flipped at bit %d", orig, pos)
 			}
-		}
-	}
-}
-
-// CompareDecoder compares decode error conditions between go-abi and go-ethereum.
-// It tests that both libraries validate inputs the same way by toggling each bit
-// in the input and verifying that both decoders either succeed or fail together.
-func CompareDecoder(t *testing.T, tuple abi.Tuple, abiSpec ethabi.ABI, method string, input []byte) {
-	// For each bit position, toggle that bit and test both decoders
-	bits := len(input) * 8
-	for i := 0; i < bits; i++ {
-		// Clone input and toggle bit i
-		In := make([]byte, len(input))
-		copy(In, input)
-		inIndex := i / 8
-		bitIndex := i % 8
-		In[inIndex] ^= 1 << bitIndex
-
-		// Test go-abi decode
-		_, err1 := tuple.Decode(In)
-
-		// Test go-ethereum unpack
-		_, err2 := abiSpec.Unpack(method, In)
-
-		// Assert that both either succeed or fail together
-		// err1 and err2 should both be nil or both be non-nil
-		hasErr1 := err1 != nil
-		hasErr2 := err2 != nil
-
-		if hasErr1 != hasErr2 {
-			t.Errorf("Bit %d: error mismatch - go-abi: %v, go-ethereum: %v", i, err1, err2)
 		}
 	}
 }
